5ebd141eaca52828ad6adf773b0544ed53baa54b
Heydar Ismayilli <heyderismayilli092@gmail.com>
2026-07-09 22:05:06 +0400

    feature added to display the selected text in a window

--- src/main.py
+++ src/main.py
@@ -5,8 +5,16 @@ gi.require_version("Gtk", "4.0")
 import pyatspi
 import os
 import traceback
+import gettext
 
-GLADE_FILE = os.path.join(os.path.dirname(os.path.abspath(__file__)), "../ui/MainWindow.ui")
+# translate program
+APP = "smartactions"
+LOCALE_DIR = "/usr/share/locale"
+gettext.bindtextdomain(APP, LOCALE_DIR)
+gettext.textdomain(APP)
+_ = gettext.gettext
+
+GLADE_FILE = os.path.join(os.path.dirname(os.path.abspath(__file__)), "../ui/MainWindow.ui")  # interface file
 
 class smartactions(Gtk.Application):
     def __init__(self):
@@ -17,8 +25,9 @@ class smartactions(Gtk.Application):
         builder.add_from_file(GLADE_FILE)
 
         # -------Widget references-------
-        self.mainwindow = builder.get_object("mainwindow")
+        self.mainwindow = builder.get_object("main_window")
         self.mainwindow.set_application(self)
+        self.choosedtext_label = builder.get_object("choosedtext_label")
         self.register_atspi()
 
     def register_atspi(self):
@@ -31,7 +40,7 @@ class smartactions(Gtk.Application):
                 start, end = text_iface.getSelection(0)
                 text = text_iface.getText(start, end)
                 print("Choosed text:", repr(text))
-                GLib.idle_add(self.show_mainwindow)  # move GTK calls to the main thread
+                GLib.idle_add(self.show_mainwindow, text)  # move GTK calls to the main thread
             except Exception:
                 traceback.print_exc()  # errors are being writing to the traceback.
 
@@ -40,8 +49,9 @@ class smartactions(Gtk.Application):
             "object:text-caret-moved"
         )
 
-    def show_mainwindow(self):
+    def show_mainwindow(self, text):
         try:
+            self.choosedtext_label.set_label(_("Choosed text: ")+text)  # print text to window
             self.mainwindow.present()
         except Exception:
             traceback.print_exc()
@@ -50,3 +60,4 @@ class smartactions(Gtk.Application):
 
 app = smartactions()
 app.run()
+
